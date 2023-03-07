16a5a1f266b39b4137c1175bd4a6d6d5f6408075
Thomas Verhage <thomasverhage@outlook.com>
2023-03-07 13:13:16 +0100

    Footer en navbar

---
+++ Navbar.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #DEB887;
+}
+
+li {
+    float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #AFEEEE;
+}
+
+</style>
+
+<head>
+
+<ul>
+<li><a class="active" href="#">Thuispagina</a></li>
+  <li><a href="#">Nieuw!</a></li>
+  <li><a href="#">Aanbieding</a></li>
+  <li><a href="#">Categorie</a></li>
+
+
+<nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Zoeken" aria-label="search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken</button>
+  </form>
+  </nav>
+
+<nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Postcode" aria-label="search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Postcode</button>
+  </form>
+  </nav>
+
+<nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Afstand" aria-label="search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Afstand</button>
+  </form>
+  </nav>
+  </ul>
+
+</head>
+
+</html>
