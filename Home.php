586a1be4d1542fdda55b70cbc5889a75d004ccd4
Thomas Verhage <thomasverhage@outlook.com>
2023-03-09 12:47:09 +0100

    Home

---
+++ Home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<style>
+body {
+  background-color: lightblue;
+}
+
+h1 {
+  color: black;
+}
+
+h2 {
+   color: darkblue;
+}
+p {
+  color: black;
+}
+
+</style>
+
+<header>
+<title>Home</title>
+
+<?php include("Navbar.php"); ?>
+
+<center>
+    <h1>Game Marktplaats</h1>
+    <h2>Koop de coolste games!</h2>
+</center>
+</header>
+
+
+<body>
+​<img src="###" alt="Foto van een videogames" width="500" height="500">
+</body>
+
+<?php include("Footer.php"); ?>
+
+</html>
