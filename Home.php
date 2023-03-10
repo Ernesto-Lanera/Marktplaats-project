e3a31f8ab77c3cea8feb729766d4b65c04233240
SMDSTEAL <258288@cursist.rockopnh.nl>
2023-03-10 10:05:13 +0100

    include fix

--- Home.php
+++ Home.php
@@ -1,55 +1,31 @@
 <!DOCTYPE html>
 <html>
-
-<style>
-header {
-  padding: 15px;
-  text-align: center;
-  background: #DEB887;
-  color: white;
-  font-size: 30px;
-}
-
-body {
-  background-color: lightgrey;
-}
-
-h1 {
-  color: black;
-}
-
-h2 {
-   color: darkblue;
-}
-p {
-  color: black;
-}
-</style>
-
 <header>
-<title>Home</title>
-
-<?php include("Navbar.php"); ?>                                                                                     <!--Hier hulp nodig met navbar invoegen-->
-
-<center>
+  <title>Home</title>
+  <link rel="stylesheet" type="text/css" href="/main.css">
+  <?php
+  include("include/Navbar.php");
+  ?>
+  <center>
     <h1>Game Marktplaats</h1>
     <h2>Koop de coolste games!</h2>
-</center>
+  </center>
 </header>
 
-
 <body>
-<center>
-​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
-<br>
-</br>
-​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
-<br>
-</br>
-​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
-</center>
+  <center>
+    ​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
+    <br>
+    </br>
+    ​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
+    <br>
+    </br>
+    ​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
+  </center>
 </body>
 
-<?php include("Footer.php"); ?>                                                                         <!--Hier hulp nodig met footer invoegen-->
+<?php
+include("include/Footer.php");
+?>
 
 </html>
