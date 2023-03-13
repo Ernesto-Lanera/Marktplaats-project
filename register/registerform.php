52fc95edb340200772c93fea6a4c2856bc36b9ed
SMDSTEAL <258288@cursist.rockopnh.nl>
2023-03-13 14:10:39 +0100

    mapping changes

--- register/registerform.php
+++ register/registerform.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Register</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
+		<link href="/css/register.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<div class="register">
