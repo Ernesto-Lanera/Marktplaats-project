666310900361683a4dc3f2409e909aba3175e454
Jamie99990 <258430@cursist.rockopnh.nl>
2023-03-13 13:26:02 +0100

    connectiondb

--- contact/contactdb.php
+++ contact/contactdb.php
@@ -1,9 +1,18 @@
 <?php
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASSWORD","");
-define("DB_NAME","marktplaats");
-$mysqli = new mysqli(DB_HOST,DB_USER, DB_PASSWORD,DB_NAME);
+$servername = "20.224.252.175";
+$username = "marktplaats4b";
+$password = "password";
+$database = "marktplaats";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 use LDAP\Result;
 extract($_POST);
 $sql = "INSERT INTO `contact-data`(`name`,`email`, `message`) VALUES ('".$name."','".$email."','".$message."')";
