969247684c5242ea346e0a17c09e1cae8f7daaac
Thomas Verhage <thomasverhage@outlook.com>
2023-03-17 09:22:56 +0100

    Index to home

--- Home.php
+++ Home.php
@@ -4,7 +4,7 @@
   <title>Home</title>
   <link rel="stylesheet" type="text/css" href="/main.css">
   <?php
-  include("include/Navbar.php");
+  include("includes/navbar.php");
   ?>
   <center>
     <h1>Game Marktplaats</h1>
@@ -14,18 +14,20 @@
 
 <body>
   <center>
-    ​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
+  ​<img src="image/1.jpg" alt="Hier foto van een videogame" width="600" height="300">
     <br>
     </br>
-    ​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
+    ​<img src="image/2.jpg" alt="Hier foto van een videogame" width="600" height="300">
     <br>
     </br>
-    ​<img src="###" alt="Hier foto van een videogame" width="500" height="500">
+    ​<img src="image/3.jpg" alt="Hier foto van een videogame" width="600" height="300">
   </center>
+  <br>
+</br>
 </body>
 
 <?php
-include("include/Footer.php");
+include("includes/footer.php");
 ?>
 
 </html>
