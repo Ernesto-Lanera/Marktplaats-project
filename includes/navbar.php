70c253d7f18ea0b717cddc62fd60d98b89f03524
SMDSTEAL <258288@cursist.rockopnh.nl>
2023-03-24 07:21:38 +0100

    catagorieen en navbar aangepast

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,48 +1,55 @@
 <link rel="stylesheet" href="./css/navbar.css">
 <div class="navbar">
-<div class="orange_bar"></div>
+  <div class="orange_bar"></div>
 
-<div class="top_bar">
-  <div class="user_info">
-  <div class="logo"></div>
-    <a href="">Help en info</a>
-    <a href="">Voorwaarden</a>
-    <a href="">Veiligheidscentrum</a>
+  <div class="top_bar">
+    <div class="user_info">
+      <div class="logo"></div>
+      <a href="">Help en info</a>
+      <a href="">Voorwaarden</a>
+      <a href="">Veiligheidscentrum</a>
+    </div>
+
+    <div class="user_navigation">
+      <a href=""><i class="fa-regular nav_icon fa-comments"></i>Berichten</a>
+      <a href=""><i class="fa-regular nav_icon fa-bell"></i>Meldingen</a>
+      <a href=""><i class="fa-regular nav_icon fa-user"></i>Inloggen</a>
+      <div class="publish_article"><i class="fa-solid nav_icon_white fa-thumbtack"></i>Plaats advertentie</div>
+    </div>
+  </div>
+  <div class="filter_bar">
+    <input type="search" placeholder="">
+    <select name="categoryId" id="categorie">
+      <option value="" disabled="">Kies categorie:</option>
+      <option selected="" value="0">Alle categorieën…</option>
+      <option value="1">Shooter</option>
+      <option value="2">Racing</option>
+      <option value="3">MMORPG</option>
+      <option value="4">Fighting</option>
+      <option value="5">Action-adventure</option>
+      <option value="6">Simulation and Sports</option>
+      <option value="7">Puzzels and Party games</option>
+      <option value="8">Strategy</option>
+      <option value="9">Platformers</option>
+      <option value="10">Other</option>
+    </select>
+    <input type="text" placeholder="Postcode">
+    <select id="afstand" name="afstand">
+      <option value="10KM">10KM</option>
+      <option value="15KM">15KM</option>
+      <option value="20KM">20KM</option>
+      <option value="20KM >">20KM ></option>
+    </select>
+    <button type="submit">zoek</button>
   </div>
 
-  <div class="user_navigation">
-    <a href=""><i class="fa-regular nav_icon fa-comments"></i>Berichten</a>
-    <a href=""><i class="fa-regular nav_icon fa-bell"></i>Meldingen</a>
-    <a href=""><i class="fa-regular nav_icon fa-user"></i>Inloggen</a>
-    <div class="publish_article"><i class="fa-solid nav_icon_white fa-thumbtack"></i>Plaats advertentie</div>
+  <div class="categories">
+    <div class="categorie_main">Alle categorieën</div>
+    <div class="categorie">Shooter</div>
+    <div class="categorie">MMORPG</div>
+    <div class="categorie">Racing</div>
+    <div class="categorie">Platformers</div>
+    <div class="categorie">Action-adventure</div>
+    <div class="categorie">Fighting</div>
   </div>
-</div>
-<div class="filter_bar">
-<input type="search" placeholder="">
-<select id="categorie" name="categorie">
-<option value="shooter">Alle categorieën</option>
-  <option value="shooter">shooter</option>
-  <option value="racing">racing</option>
-  <option value="fighter">fighter</option>
-  <option value="MMORPG">MMORPG</option>
-</select>
-<input type="text" placeholder="Postcode">
-<select id="afstand" name="afstand">
-  <option value="10KM">10KM</option>
-  <option value="15KM">15KM</option>
-  <option value="20KM">20KM</option>
-  <option value="20KM >">20KM ></option>
-</select>
-<button type="submit">zoek</button>
-</div>
-
-<div class="categories">
-  <div class="categorie_main">DUMMY alle</div>
-  <div class="categorie">DUMMY</div>
-  <div class="categorie">DUMMY</div>
-  <div class="categorie">DUMMY</div>
-  <div class="categorie">DUMMY</div>
-  <div class="categorie">DUMMY</div>
-  <div class="categorie">DUMMY</div>
-</div>
 </div>
