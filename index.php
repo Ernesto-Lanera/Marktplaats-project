62d5cea171be44f074bd5eafcb5d7400fa8caa1c
SMDSTEAL <258288@cursist.rockopnh.nl>
2023-03-31 11:09:29 +0200

    Revert "index .php deleten"
    This reverts commit d0a491cb7f2ab1a9555620af799c5c20cbce7bbd.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <title>Home</title>
+  <link rel="stylesheet" type="text/css" href="/main.css">
+  <?php
+  include("includes/navbar.php");
+  ?>
+  <center>
+    <h1>Game Marktplaats</h1>
+    <h2>Koop de coolste games!</h2>
+  </center>
+</header>
+
+<body>
+
+  <br>
+</br>
+</body>
+
+<?php
+include("includes/footer.php");
+?>
+
+</html>
