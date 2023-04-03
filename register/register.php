83ee39930a4eaecdb15d7db6103fdf6760ad717f
Jamie99990 <258430@cursist.rockopnh.nl>
2023-04-03 11:41:24 +0200

    redirect+message
    Co-authored-by: Thijs Molenaar <TopdevT@users.noreply.github.com>

--- register/register.php
+++ register/register.php
@@ -2,10 +2,11 @@
 // Change this to your connection info.
 $DATABASE_HOST = '20.224.252.175';
 $DATABASE_USER = 'marktplaats4b';
-$DATABASE_PASS = 'password';
+$DATABASE_PASS = 'root';
 $DATABASE_NAME = 'marktplaats';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
 if (mysqli_connect_errno()) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
@@ -35,11 +36,14 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE user_name = ?
 if ($stmt = $con->prepare('INSERT INTO accounts (user_name, password, user_Email, telephone, postal_Code, user_Location) VALUES (?, ?, ?, ?, ?, ?)')) {
 	// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
 	$password = ($_POST['password']);
-	if ($_POST['password'] === $password) {	
-	}
-	$stmt->bind_param('ssssss', $_POST['user_name'], $password, $_POST['user_Email'], $_POST['telephone'], $_POST['user_Location'], $_POST['postal_Code']);
+	if ($_POST['password'] === $password) {
+		$stmt->bind_param('ssssss', $_POST['user_name'], $password, $_POST['user_Email'], $_POST['telephone'], $_POST['user_Location'], $_POST['postal_Code']);
 	$stmt->execute();
-	header("Location: ../login/login.php");
+	echo ("<script LANGUAGE='JavaScript'>
+    window.alert('succesvol geregistreerd');
+    window.location.href='../login/login.php';
+    </script>");
+	}
 } else {
 	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
 	echo 'Could not prepare statement!';
