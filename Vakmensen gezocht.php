c2e71eded2fc0cc445221e1a864eee93abff9a09
Thomas Verhage <thomasverhage@outlook.com>
2023-04-06 16:09:32 +0200

    Vakmensen gezocht

---
+++ Vakmensen gezocht.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <title>Vakmensen gezocht</title>
+  <link rel="stylesheet" type="text/css" href="/main.css">
+  <?php
+  include("includes/navbar.php");
+  ?>
+</header>
+
+<body>
+
+<h1>Vakmensen gezocht</h1>
+
+<br>
+</br>
+
+<p>* Game designer gezocht</p>
+<p>* Video game tester gezocht</p>
+<p>* ICT'er met kennis van Unreal Engine gezocht</p>
+<p>* Student met kennis van HTML/CSS gezocht. (minimaal MBO 3 opzak.)</p>
+<p>* App developer gezocht (Ervaring met Python/Javascript)</p>
+  <br>
+  </br>
+  <p>Bij intresse stuur een e-mail.</p>
+	<a href = "mailto: vakmensen@mail.com">Stuur E-mail</a>
+    <br>
+</body>
+
+<?php
+include("includes/footer.php");
+?>
+
+</html>
