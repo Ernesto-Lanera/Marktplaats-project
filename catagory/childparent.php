0659bec6a842db30e6f9ddc750c365ce90e4c4c9
Ernesto Lanera <258288@cursist.rockopnh.nl>
2023-04-13 20:02:54 +0200

    comments changes

--- catagory/childparent.php
+++ catagory/childparent.php
@@ -2,13 +2,20 @@
 // Include the database configuration file
 require 'dbConfig.php';
 
-function categoryTree($parent_id = 0, $sub_mark = ''){
+// Function to build a category tree
+function categoryTree($parent_id, $sub_mark){
+    // Connect to the database
     global $db;
+    // Run the query to get all the categories from the database
     $query = $db->query("SELECT * FROM categories WHERE parent_id = $parent_id ORDER BY name ASC");
    
+    // Check if there are any categories returned
     if($query->num_rows > 0){
+        // Loop through the categories
         while($row = $query->fetch_assoc()){
+            // Print out the category name and ID
             echo '<option value="'.$row['id'].'">'.$sub_mark.$row['name'].'</option>';
+            // Run the function again but pass in the ID of the current category as the parent ID
             categoryTree($row['id'], $sub_mark.'---');
         }
     }
--- catagory/dropdown.php
+++ catagory/dropdown.php
@@ -1,3 +1,20 @@
-<select name="category">
-    <?php categoryTree(); ?>
-</select>
+<?php
+    // this is a PHP function.  It will be used to create the HTML
+    // for the category drop-down list
+    function categoryTree() {
+        // create a database connection
+        $db = new PDO('mysql:host=localhost;dbname=ecommerce', '
+        root', '');
+        // create a query to get all the categories from the database
+        $query = $db->query("SELECT * FROM categories WHERE parent_id = 0 ORDER BY name ASC");
+        // check if there are any categories returned
+        if($query->rowCount() > 0) {
+            // loop through the categories
+            while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                // print out the category name and ID
+                echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+                // run the function again but pass in the ID of the current category as the parent ID
+                categoryTree($row['id'], '---');
+            }
+        }
+    }
