d81a5bcccf6457f3efa2a47ef7edcd7e6ac772c3
Thomas Verhage <thomasverhage@outlook.com>
2023-04-13 12:45:38 +0200

    Vakmensen gezocht aanpassing

--- Vakmensen gezocht.php
+++ Vakmensen gezocht.php
@@ -15,22 +15,51 @@
 <br>
 </br>
 
-<p>* Game designer gezocht</p>
+
+<h1>Vacatures</h1>
+<p>* Game designer gezocht</p>                                                                                    <!--body met informatie over vacatures-->
+<hr>
 <p>* Video game tester gezocht</p>
+<hr>
 <p>* ICT'er met kennis van Unreal Engine gezocht</p>
-<p>* Student met kennis van HTML/CSS gezocht. (minimaal MBO 3 opzak.)</p>
+<hr>
+<p>* Student met kennis van HTML/CSS gezocht. (Minimaal MBO 3 opzak.)</p>
+<hr>
 <p>* App developer gezocht (Ervaring met Python/Javascript)</p>
+<hr>
+<p>* Applicatiebeheerder (MBO 4 of HBO)</p>
   <br>
   </br>
   <p>Bij intresse stuur een e-mail.</p>
 	<a href = "mailto: vakmensen@mail.com">Stuur E-mail</a>
-    <br></br>
+    <br>
+  </br>
 
+<p>Of bel naar: Bel: 06 1234567</p>
 
 <br>
 </br>
 
+                                                                                                            <!--bruine info box style (HTML class)-->
+<style>                                                                                                           
+.Colorbox {
+  background-color: BurlyWood;
+  color: white;
+  border: 2px solid black;
+  margin: 80px;
+  padding: 25px;
+}
+</style>
+
+<div class="Colorbox">                                                                                       <!--bruine info box (HTML class)-->
+<h1>Belangrijk</h1>
+<br>
+</br>
+<h2>We zijn opzoek naar hardwerkende enthousiaste collega's.</h2>
+<h2>Ben je bereid 32 tot 40 uur per week te werken.<h2>
+<h2>Ben jij dat? Laat het ons weten.<h2>
 <h2>Dankjewel voor u interesse.</h2>
+</div>
 
 </body>
 
